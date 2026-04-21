b06a3a1b699434ffbd067642899774835c1da05f
Ethan Guerin <129787373+EthanAGi@users.noreply.github.com>
2026-04-21 10:24:16 -0700

    Add: Attempt to add UI Inputs for Level Screen

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -9,6 +9,22 @@ func _ready() -> void:
 	back_button.pressed.connect(_on_back_pressed)
 
 	_setup_menu_focus()
+	call_deferred("_focus_default_button")
+
+func _process(_delta: float) -> void:
+	_ensure_focus()
+
+	if Input.is_action_just_pressed("ui_up"):
+		_focus_previous_button()
+		return
+
+	if Input.is_action_just_pressed("ui_down"):
+		_focus_next_button()
+		return
+
+	if Input.is_action_just_pressed("ui_accept"):
+		_press_focused_button()
+		return
 
 func _setup_menu_focus() -> void:
 	test_level_button.focus_mode = Control.FOCUS_ALL
@@ -23,7 +39,53 @@ func _setup_menu_focus() -> void:
 	test_level_button.mouse_entered.connect(func() -> void: test_level_button.grab_focus())
 	back_button.mouse_entered.connect(func() -> void: back_button.grab_focus())
 
-	test_level_button.grab_focus()
+func _focus_default_button() -> void:
+	if test_level_button != null:
+		test_level_button.grab_focus()
+
+func _ensure_focus() -> void:
+	var focused: Control = get_viewport().gui_get_focus_owner()
+	if focused == null:
+		test_level_button.grab_focus()
+
+func _focus_previous_button() -> void:
+	var focused: Control = get_viewport().gui_get_focus_owner()
+
+	if focused == null:
+		test_level_button.grab_focus()
+		return
+
+	if focused == back_button:
+		test_level_button.grab_focus()
+	else:
+		back_button.grab_focus()
+
+func _focus_next_button() -> void:
+	var focused: Control = get_viewport().gui_get_focus_owner()
+
+	if focused == null:
+		test_level_button.grab_focus()
+		return
+
+	if focused == test_level_button:
+		back_button.grab_focus()
+	else:
+		test_level_button.grab_focus()
+
+func _press_focused_button() -> void:
+	var focused: Control = get_viewport().gui_get_focus_owner()
+
+	if focused == null:
+		test_level_button.grab_focus()
+		return
+
+	if focused == test_level_button:
+		test_level_button.emit_signal("pressed")
+		return
+
+	if focused == back_button:
+		back_button.emit_signal("pressed")
+		return
 
 func _on_test_level_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
